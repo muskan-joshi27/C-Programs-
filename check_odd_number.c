493bfb92e319415b31b9840e9e0f50680fcca88b
Neeraj Pareek <101200487+NightCoder11@users.noreply.github.com>
2022-10-13 23:44:20 +0530

    check_odd_number

---
+++ check_odd_number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",%n);
+    if(n%2!==0)
+    {
+        printf("ODD");
+    }
+    else
+    {
+        printf("Not ODD");
+    }
+    return 0;
+}
